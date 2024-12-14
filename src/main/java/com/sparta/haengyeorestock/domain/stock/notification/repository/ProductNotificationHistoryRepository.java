c954b67796736fc0dca2b75829d2d07e795808fd
Choi-Hyeok99 <gur0709@naver.com>
2024-12-14 16:37:47 +0900

    feat : ProductRepository,ProductUserNotificationRepository,ProductNotificationHistoryRepository 생성

---
+++ src/main/java/com/sparta/haengyeorestock/domain/stock/notification/repository/ProductNotificationHistoryRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.haengyeorestock.domain.stock.notification.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductNotificationHistoryRepository {
+}
